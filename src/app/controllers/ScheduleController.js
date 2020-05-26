73b12a142a7f023db29d0bf5b2d7d0834087c87d
BrenoHA <breno.ha@gmail.com>
2020-05-26 01:53:43 -0300

    Index Schedule

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,6 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -11,7 +14,26 @@ class ScheduleController {
       return res.status(401).json({ error: 'User is not a provider' });
     }
 
-    return res.json();
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    // 2020-05-27T11:00:00
+
+    // 2020-05-27T00:00:00
+    // 2020-05-27T23:59:59
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
   }
 }
 
